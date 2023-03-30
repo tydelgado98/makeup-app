8b0408219d30940302aa05e9de069bcbadac5ad2
unknown <delgadojustin100@yahoo.com>
2023-03-30 02:35:01 -0400

    made the site intro

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Enchanted Greatness
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More On React
         </a>
       </header>
     </div>
