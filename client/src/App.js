35e56ce1943ead59b59b71769501144f5ec1b696
unknown <delgadojustin100@yahoo.com>
2023-03-30 04:03:24 -0400

    made changes to the app.js]

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router} from 'react-router-dom';
+
+//Routes, Route <-- This would go after the Router up there ^^^^
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
+      <Router>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +56,8 @@ function App() {
         </a>
       </header>
     </div>
+    </Router>
+    </ApolloProvider>
   );
 }
 
